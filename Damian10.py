f0d75bb69b3852eb260a5eea7706a799d436173a
DamianRud <damianrud257@gmail.com>
2025-09-24 11:32:48 +0300

    Add files via upload

---
+++ Damian10.py
@@ -0,0 +1,69 @@
+#hour = int(input("Sisesta tund (0-23):"))
+
+#if hour <6:
+    #print("ÖÖ")
+#else:
+    #if hour < 12:
+        #print("Hommik")
+   # else:
+       # if hour < 18:
+            #print("päev")
+        #else:
+           # print("Õhtu")
+
+
+#preference = input("Sisesta eelistus (Kuum/Külm):")
+#if preference == "Kuum":
+    #tüüp = input ("Sisesta tüüp (tee/kohv):")
+    #if tüüp == "tee":
+        #print("Valmistamine teed")
+    #else:
+        #print("Valmistamine kohvi")
+#else:
+    #print("Limonad")
+       
+
+
+#color = input("Sisesta colour")
+
+#if color == "punane":
+    #print("Ere")
+#else:
+    #if color == "Sinine":
+        #print("Rahulik")
+    #else:
+        #print("Tavaline")
+   
+   
+   
+   
+   
+   
+   
+   
+   
+#transport = input("Sisesta transport (buss/jalgsi):")
+#weather = input("Sisesta ilm(vihm/päike):")
+
+#if transport == "buss":
+    #if weather == "vihm":
+        #print("sõidame bussiga katuse alla!")
+    #else:
+        #print("sõidame bussiga mugavalt")
+#else:
+    #if weather == "vihm":
+        #print("võtame vihmavarju")
+    #else:
+        #print("läheme jalgsi")
+
+
+
+
+
+
+
+
+
+
+
+
